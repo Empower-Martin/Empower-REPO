d9e7e0a59cdcb5bbebcc5c7cc8b541b388cbea48
Martin <Martin@MARTIN>
2023-03-28 14:24:50 +0200

    Add project files.

---
+++ EmpowerML_1/Properties/AssemblyInfo.cs
@@ -0,0 +1,16 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("EmpowerML_1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Skyline Communications")]
+[assembly: AssemblyProduct("EmpowerML_1")]
+[assembly: AssemblyCopyright("Copyright © Skyline Communications")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+[assembly: Guid("786CF357-56E0-4AE3-B709-A8FD7535B1E4")]
+[assembly: AssemblyVersion("1.0.0.0")]
