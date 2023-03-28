9f1e822d9d1176824fc8514d6ca0cc85106e4857
Martin <Martin@MARTIN>
2023-03-28 16:37:17 +0200

    333

--- EmpowerML_1/EmpowerML_1.cs
+++ EmpowerML_1/EmpowerML_1.cs
@@ -56,6 +56,7 @@ namespace EmpowerML_1
 	using System.Globalization;
 	using System.Text;
 	using Skyline.DataMiner.Automation;
+	using Skyline.DataMiner.Core.DataMinerSystem.Automation;
 
 	/// <summary>
 	/// Represents a DataMiner Automation script.
@@ -69,6 +70,11 @@ namespace EmpowerML_1
 		public void Run(IEngine engine)
 		{
 			engine.GenerateInformation("Hello world!");
+			var myDms = engine.GetDms();
+			foreach (var element in myDms.GetElements())
+			{
+				engine.GenerateInformation(element.Name);
+			}
 			//changes aa
 		}
 	}
